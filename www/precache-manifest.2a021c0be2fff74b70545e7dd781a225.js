de3093b3d6d8c8d39ecaa5e97f0cfe735478394f
Zefau <zefau@mailbox.org>
2020-03-08 21:34:44 +0100

    v0.0.2

---
+++ www/precache-manifest.2a021c0be2fff74b70545e7dd781a225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4585060c77599f196b49378b1dc73a89",
+    "url": "/jarvis/index.html"
+  },
+  {
+    "revision": "a192f600f29a73448fdf",
+    "url": "/jarvis/static/css/main.680b29e9.chunk.css"
+  },
+  {
+    "revision": "02d613a5133b4d7d848e",
+    "url": "/jarvis/static/js/2.c8f7f9eb.chunk.js"
+  },
+  {
+    "revision": "16de47a2529d5b706b5d8250d732ef18",
+    "url": "/jarvis/static/js/2.c8f7f9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a192f600f29a73448fdf",
+    "url": "/jarvis/static/js/main.da7046ee.chunk.js"
+  },
+  {
+    "revision": "699fcb0b99f70f2457f4",
+    "url": "/jarvis/static/js/runtime-main.d7c12313.js"
+  }
+]);
