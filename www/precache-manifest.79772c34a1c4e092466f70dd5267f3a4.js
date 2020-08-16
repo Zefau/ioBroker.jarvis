10a64108eff91896f0d61a30044dcde8aeff4382
Zefau <zefau@mailbox.org>
2020-08-16 23:10:36 +0200

    v1.0.0-alpha.2

---
+++ www/precache-manifest.79772c34a1c4e092466f70dd5267f3a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0091997f6561954758a90c6ad7c07935",
+    "url": "/jarvis/index.html"
+  },
+  {
+    "revision": "6c5308195dd707d8a4d2",
+    "url": "/jarvis/static/css/main.680b29e9.chunk.css"
+  },
+  {
+    "revision": "e98679fad1754bea62fa",
+    "url": "/jarvis/static/js/2.04b3526b.chunk.js"
+  },
+  {
+    "revision": "7dd561714315aab168f1020116c6c1d9",
+    "url": "/jarvis/static/js/2.04b3526b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0518dc7365c6129514c",
+    "url": "/jarvis/static/js/3.af9fa8b2.chunk.js"
+  },
+  {
+    "revision": "6c5308195dd707d8a4d2",
+    "url": "/jarvis/static/js/main.c85f7bb4.chunk.js"
+  },
+  {
+    "revision": "5293a7466b79e8686046",
+    "url": "/jarvis/static/js/runtime-main.52f75274.js"
+  },
+  {
+    "revision": "385366192a426d4c299b92c45bd9381b",
+    "url": "/jarvis/static/media/AdapterStatus1.38536619.png"
+  },
+  {
+    "revision": "cb44dbbf4f0c025ce205a1f0981c62e9",
+    "url": "/jarvis/static/media/Chart1.cb44dbbf.png"
+  },
+  {
+    "revision": "7819d286244806865fca917c3efef552",
+    "url": "/jarvis/static/media/StateList2.7819d286.png"
+  },
+  {
+    "revision": "5695145e384edeb75d81aabd79983dea",
+    "url": "/jarvis/static/media/StateList4.5695145e.png"
+  }
+]);
