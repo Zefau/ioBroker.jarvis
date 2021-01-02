011407ef70661424211ac281febf74532a335772
Zefau <zefau@mailbox.org>
2021-01-03 00:02:41 +0100

    v2.2.0-beta.2

--- www/service-worker.js
+++ www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jarvis/precache-manifest.a1f6baed3f22878bf635e52a870ea62f.js"
+  "/jarvis/precache-manifest.d09263fb6e85c3ad151b1085add5e5eb.js"
 );
 
 self.addEventListener('message', (event) => {
