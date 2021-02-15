0aa4fba8cba385db6de794f7c88f9de427f6d1c6
Zefau <zefau@mailbox.org>
2021-02-15 22:36:29 +0100

    v2.2.0-beta.42

---
+++ www/precache-manifest.215b40b32981b28ce85a263bafb84b29.js
@@ -0,0 +1,546 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7743cae62cb2ca4589976193ad2d3a",
+    "url": "/jarvis/index.html"
+  },
+  {
+    "revision": "c241d635be92528cac36",
+    "url": "/jarvis/static/css/3.830695a1.chunk.css"
+  },
+  {
+    "revision": "e9dd733f93461efd3ba9",
+    "url": "/jarvis/static/css/main.f3e4a4d9.chunk.css"
+  },
+  {
+    "revision": "c4e2c2e76f613a602958",
+    "url": "/jarvis/static/js/0.6d4ea919.chunk.js"
+  },
+  {
+    "revision": "b790df81c29f7d0ecea3",
+    "url": "/jarvis/static/js/1.c2ab2b35.chunk.js"
+  },
+  {
+    "revision": "65a2bcbd313f07e0e02f",
+    "url": "/jarvis/static/js/11.9c779b35.chunk.js"
+  },
+  {
+    "revision": "90307b4f87a866702a4ef7582b60692f",
+    "url": "/jarvis/static/js/11.9c779b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09640e6d466d49a1f0d8",
+    "url": "/jarvis/static/js/12.b4767522.chunk.js"
+  },
+  {
+    "revision": "d985de5d55e0e6dbadee53aa909f70c1",
+    "url": "/jarvis/static/js/12.b4767522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61af63106d8d77eeba08",
+    "url": "/jarvis/static/js/13.ce5599f9.chunk.js"
+  },
+  {
+    "revision": "d985de5d55e0e6dbadee53aa909f70c1",
+    "url": "/jarvis/static/js/13.ce5599f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f2ec4b99f1f37533ee",
+    "url": "/jarvis/static/js/14.993a2d2b.chunk.js"
+  },
+  {
+    "revision": "f69fe68655adb99c809f",
+    "url": "/jarvis/static/js/15.c8a78051.chunk.js"
+  },
+  {
+    "revision": "54d4a419c7bc354078d7",
+    "url": "/jarvis/static/js/16.c78f8fff.chunk.js"
+  },
+  {
+    "revision": "3983f9ed39ebe0117f70",
+    "url": "/jarvis/static/js/17.12315a8e.chunk.js"
+  },
+  {
+    "revision": "38b0886c576bf044847c",
+    "url": "/jarvis/static/js/18.fa7f6a6d.chunk.js"
+  },
+  {
+    "revision": "8e09aeb9a730a179e933",
+    "url": "/jarvis/static/js/19.e5b714ae.chunk.js"
+  },
+  {
+    "revision": "d6ce8ebb224ecbae67bc",
+    "url": "/jarvis/static/js/2.dae26556.chunk.js"
+  },
+  {
+    "revision": "30b61815394cad44625e",
+    "url": "/jarvis/static/js/20.bf128552.chunk.js"
+  },
+  {
+    "revision": "67acc4f16ae90ea97a98",
+    "url": "/jarvis/static/js/21.51ff8d96.chunk.js"
+  },
+  {
+    "revision": "7dd961223f7fd838f5aa",
+    "url": "/jarvis/static/js/22.381e5276.chunk.js"
+  },
+  {
+    "revision": "f593bc5b0229923bffd7",
+    "url": "/jarvis/static/js/23.2ed9d4aa.chunk.js"
+  },
+  {
+    "revision": "3a3bebb719b6799bc834",
+    "url": "/jarvis/static/js/24.bfa9aac9.chunk.js"
+  },
+  {
+    "revision": "9553f040c230b71f8770",
+    "url": "/jarvis/static/js/25.653570d6.chunk.js"
+  },
+  {
+    "revision": "cbdc289e8f27cfa22a54",
+    "url": "/jarvis/static/js/26.dd11772a.chunk.js"
+  },
+  {
+    "revision": "e9529d847a6ff49a2360",
+    "url": "/jarvis/static/js/27.24b89e56.chunk.js"
+  },
+  {
+    "revision": "5b5a164b8deef863e05c",
+    "url": "/jarvis/static/js/28.f1a4e554.chunk.js"
+  },
+  {
+    "revision": "28f32201e71c29e30111",
+    "url": "/jarvis/static/js/29.edd9d6de.chunk.js"
+  },
+  {
+    "revision": "c241d635be92528cac36",
+    "url": "/jarvis/static/js/3.a3b0345d.chunk.js"
+  },
+  {
+    "revision": "fcec7dc208802e67f3df84d95571297f",
+    "url": "/jarvis/static/js/3.a3b0345d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff7482995be22530521",
+    "url": "/jarvis/static/js/30.fb8216c9.chunk.js"
+  },
+  {
+    "revision": "afcab54f182aea0fe62d",
+    "url": "/jarvis/static/js/31.eb0b3f7b.chunk.js"
+  },
+  {
+    "revision": "790beff2bd030c4e1c86",
+    "url": "/jarvis/static/js/32.8f81f2d6.chunk.js"
+  },
+  {
+    "revision": "1b42221085c6782f74a9",
+    "url": "/jarvis/static/js/33.c368528a.chunk.js"
+  },
+  {
+    "revision": "2cba5c34628c5748066a",
+    "url": "/jarvis/static/js/34.a3a6182c.chunk.js"
+  },
+  {
+    "revision": "b430bf4b3f688e32c0de",
+    "url": "/jarvis/static/js/35.974f9dec.chunk.js"
+  },
+  {
+    "revision": "b5c8162115b713b55036",
+    "url": "/jarvis/static/js/36.fc473451.chunk.js"
+  },
+  {
+    "revision": "16cb2d5cd1397b484c2f",
+    "url": "/jarvis/static/js/37.504404fa.chunk.js"
+  },
+  {
+    "revision": "a5fdd65522dd9987236b",
+    "url": "/jarvis/static/js/38.7050175c.chunk.js"
+  },
+  {
+    "revision": "550d076473c1ecd8be3a",
+    "url": "/jarvis/static/js/39.571edc39.chunk.js"
+  },
+  {
+    "revision": "a159ad0fa3a8f111056d",
+    "url": "/jarvis/static/js/4.a4492157.chunk.js"
+  },
+  {
+    "revision": "d985de5d55e0e6dbadee53aa909f70c1",
+    "url": "/jarvis/static/js/4.a4492157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f229e8481bd028b9cf",
+    "url": "/jarvis/static/js/40.40a6c0d8.chunk.js"
+  },
+  {
+    "revision": "10ffa626eed65ad68a09",
+    "url": "/jarvis/static/js/41.3844a8f4.chunk.js"
+  },
+  {
+    "revision": "8d59367820c729c790df",
+    "url": "/jarvis/static/js/42.916a2c55.chunk.js"
+  },
+  {
+    "revision": "c6ad7529c6a4d211070f",
+    "url": "/jarvis/static/js/43.0ae696b2.chunk.js"
+  },
+  {
+    "revision": "20fb39273c0c6c95a7e3",
+    "url": "/jarvis/static/js/44.71219640.chunk.js"
+  },
+  {
+    "revision": "36eecf142a875780ee4d",
+    "url": "/jarvis/static/js/45.fc1c48a2.chunk.js"
+  },
+  {
+    "revision": "fbf8ed656fd513369fb0",
+    "url": "/jarvis/static/js/46.df9a5962.chunk.js"
+  },
+  {
+    "revision": "3a670d225b273653e989",
+    "url": "/jarvis/static/js/47.b5235215.chunk.js"
+  },
+  {
+    "revision": "dd4dd65145a60c542219",
+    "url": "/jarvis/static/js/48.b017ecca.chunk.js"
+  },
+  {
+    "revision": "7ebe3969c793c689911a",
+    "url": "/jarvis/static/js/49.b997cd13.chunk.js"
+  },
+  {
+    "revision": "c35dc3411533be405037",
+    "url": "/jarvis/static/js/5.2aa34628.chunk.js"
+  },
+  {
+    "revision": "b9e4cbefe5d124f3cc46",
+    "url": "/jarvis/static/js/50.1de2cfbd.chunk.js"
+  },
+  {
+    "revision": "625abe60feafbd8b2c31",
+    "url": "/jarvis/static/js/51.a9f4ea23.chunk.js"
+  },
+  {
+    "revision": "3fae08ad59888c70dcb5",
+    "url": "/jarvis/static/js/52.b2adf97c.chunk.js"
+  },
+  {
+    "revision": "1eca8f87807ae65d2348",
+    "url": "/jarvis/static/js/53.948a9ce7.chunk.js"
+  },
+  {
+    "revision": "b3c2cf52039dd5e6d2dd",
+    "url": "/jarvis/static/js/54.cfe7f373.chunk.js"
+  },
+  {
+    "revision": "c5a1be8030be9ca09512",
+    "url": "/jarvis/static/js/55.68358f42.chunk.js"
+  },
+  {
+    "revision": "ac8ef54654a1ed824a50",
+    "url": "/jarvis/static/js/56.f3c74b1f.chunk.js"
+  },
+  {
+    "revision": "d46fe000c1ab467ef016",
+    "url": "/jarvis/static/js/57.1373ec02.chunk.js"
+  },
+  {
+    "revision": "cb21a0d94a2fb79b169b",
+    "url": "/jarvis/static/js/58.a2ed5067.chunk.js"
+  },
+  {
+    "revision": "4108b4c9121aa3bed820",
+    "url": "/jarvis/static/js/59.ced98599.chunk.js"
+  },
+  {
+    "revision": "da9cb2a81fa067a0a159",
+    "url": "/jarvis/static/js/6.e7a594b2.chunk.js"
+  },
+  {
+    "revision": "35926a0968596a71bd0c6b2c2fafe2be",
+    "url": "/jarvis/static/js/6.e7a594b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c30c4f11342888d382",
+    "url": "/jarvis/static/js/60.ed054ed5.chunk.js"
+  },
+  {
+    "revision": "ffbf2cb2bd258ffac497",
+    "url": "/jarvis/static/js/61.becd7c29.chunk.js"
+  },
+  {
+    "revision": "da14bf0eaa1b4825eb67",
+    "url": "/jarvis/static/js/62.3a72ab1b.chunk.js"
+  },
+  {
+    "revision": "c5473e72fac0b42b7012",
+    "url": "/jarvis/static/js/63.307ac8e3.chunk.js"
+  },
+  {
+    "revision": "a4577b8a1034dd245165",
+    "url": "/jarvis/static/js/64.858d11e5.chunk.js"
+  },
+  {
+    "revision": "7f76ebe66d1e26c31eaf",
+    "url": "/jarvis/static/js/65.4f51e480.chunk.js"
+  },
+  {
+    "revision": "f7aba10559faa416090e",
+    "url": "/jarvis/static/js/66.0b385047.chunk.js"
+  },
+  {
+    "revision": "fc532ec8fced723e85a4",
+    "url": "/jarvis/static/js/7.4316d8b3.chunk.js"
+  },
+  {
+    "revision": "4969f784e5faf82e2ddb",
+    "url": "/jarvis/static/js/8.a4044112.chunk.js"
+  },
+  {
+    "revision": "e9dd733f93461efd3ba9",
+    "url": "/jarvis/static/js/main.8d5a4574.chunk.js"
+  },
+  {
+    "revision": "bc11002a50586234ba3a",
+    "url": "/jarvis/static/js/runtime-main.3213caf0.js"
+  },
+  {
+    "revision": "b2b907ab14ac5605e1b9d47425372323",
+    "url": "/jarvis/static/media/0.b2b907ab.png"
+  },
+  {
+    "revision": "974c7b02e165831eb6ab5cc47e766302",
+    "url": "/jarvis/static/media/1.974c7b02.png"
+  },
+  {
+    "revision": "a768f44447ce834db91c72e12d4a36ee",
+    "url": "/jarvis/static/media/1.a768f444.png"
+  },
+  {
+    "revision": "2d82c7a1f523ac7313e4e6373f85fc67",
+    "url": "/jarvis/static/media/10.2d82c7a1.png"
+  },
+  {
+    "revision": "3280e63d07c5aa649f0c47a533e8a392",
+    "url": "/jarvis/static/media/10.3280e63d.png"
+  },
+  {
+    "revision": "0ec432c3839f6cb9fd76627e4efc180d",
+    "url": "/jarvis/static/media/11.0ec432c3.png"
+  },
+  {
+    "revision": "91db3ce14f06df1846cba96df15339c8",
+    "url": "/jarvis/static/media/11.91db3ce1.png"
+  },
+  {
+    "revision": "10ac603233360262de544fc35b8f4098",
+    "url": "/jarvis/static/media/12.10ac6032.png"
+  },
+  {
+    "revision": "2b795d68100a75b7146ce4a0d51b6cc3",
+    "url": "/jarvis/static/media/12.2b795d68.png"
+  },
+  {
+    "revision": "8024002e9954a6b8cf9ff009111bfefe",
+    "url": "/jarvis/static/media/13.8024002e.png"
+  },
+  {
+    "revision": "a9217fb8d42dbfdd296ac8a2313b1a50",
+    "url": "/jarvis/static/media/13.a9217fb8.png"
+  },
+  {
+    "revision": "3bad5f750f7ed359f8f0adf9e433c297",
+    "url": "/jarvis/static/media/14.3bad5f75.png"
+  },
+  {
+    "revision": "42643627e80ab6344eaa0ad894ebf58f",
+    "url": "/jarvis/static/media/14.42643627.png"
+  },
+  {
+    "revision": "ce345857f3eaf11819c9426aef3e2481",
+    "url": "/jarvis/static/media/15.ce345857.png"
+  },
+  {
+    "revision": "f8cfa72e36bc73cfda9073166bf400d1",
+    "url": "/jarvis/static/media/15.f8cfa72e.png"
+  },
+  {
+    "revision": "09815a152acff189435afcf7d5c03bf8",
+    "url": "/jarvis/static/media/16.09815a15.png"
+  },
+  {
+    "revision": "4dbff86340023f273fb383d85d35f54a",
+    "url": "/jarvis/static/media/16.4dbff863.png"
+  },
+  {
+    "revision": "b94f9062b58ec445aa3457d42dd70028",
+    "url": "/jarvis/static/media/17.b94f9062.png"
+  },
+  {
+    "revision": "bcbb73967fa1adfeff6e93c11e063ce5",
+    "url": "/jarvis/static/media/17.bcbb7396.png"
+  },
+  {
+    "revision": "12760c61b3c35c8c3d183d38b949aaaf",
+    "url": "/jarvis/static/media/18.12760c61.png"
+  },
+  {
+    "revision": "5ac4a99536891cfe26cda3246778b665",
+    "url": "/jarvis/static/media/18.5ac4a995.png"
+  },
+  {
+    "revision": "d47160b72c8faeb4e25cd243edefb1bc",
+    "url": "/jarvis/static/media/19.d47160b7.png"
+  },
+  {
+    "revision": "fedcf1afe1f16f36ef8c10edf4babc10",
+    "url": "/jarvis/static/media/19.fedcf1af.png"
+  },
+  {
+    "revision": "140fbf476faa408fd4c17b8651c7bbc4",
+    "url": "/jarvis/static/media/2.140fbf47.png"
+  },
+  {
+    "revision": "1e46365edc2e0f5d0aed06ed98bded85",
+    "url": "/jarvis/static/media/2.1e46365e.png"
+  },
+  {
+    "revision": "182c85210d10fd870bf5dc2c6adf750d",
+    "url": "/jarvis/static/media/20.182c8521.png"
+  },
+  {
+    "revision": "c718e56c6c9410b190fc8c82ef028823",
+    "url": "/jarvis/static/media/21.c718e56c.png"
+  },
+  {
+    "revision": "e2f6abafef31bb900140eac8f3bdf9d8",
+    "url": "/jarvis/static/media/22.e2f6abaf.png"
+  },
+  {
+    "revision": "654f95e377a848a3a759c682b4588c3e",
+    "url": "/jarvis/static/media/23.654f95e3.png"
+  },
+  {
+    "revision": "8d32bfb7717ffc75f6fdee237633ac1b",
+    "url": "/jarvis/static/media/24.8d32bfb7.png"
+  },
+  {
+    "revision": "1eeb07d426fa1e5c254b9c0e13983c96",
+    "url": "/jarvis/static/media/25.1eeb07d4.png"
+  },
+  {
+    "revision": "4cead42498c539abc41fce53718fe417",
+    "url": "/jarvis/static/media/26.4cead424.png"
+  },
+  {
+    "revision": "696d186db7cea19031f3766268b12658",
+    "url": "/jarvis/static/media/27.696d186d.png"
+  },
+  {
+    "revision": "1365198f06b213c52a2560901aa9f2c6",
+    "url": "/jarvis/static/media/28.1365198f.png"
+  },
+  {
+    "revision": "5369ab938f2399695f70517b69e765ca",
+    "url": "/jarvis/static/media/29.5369ab93.png"
+  },
+  {
+    "revision": "162ee376bd0f626f97629052a9b65c29",
+    "url": "/jarvis/static/media/3.162ee376.png"
+  },
+  {
+    "revision": "bd9bb1d2fd9189c5337854067a32d27b",
+    "url": "/jarvis/static/media/3.bd9bb1d2.png"
+  },
+  {
+    "revision": "6d0ccabcff89757e2df37f9c9b12f030",
+    "url": "/jarvis/static/media/4.6d0ccabc.png"
+  },
+  {
+    "revision": "960846a622d0f0657eca9d5ace8044bf",
+    "url": "/jarvis/static/media/4.960846a6.png"
+  },
+  {
+    "revision": "9610ad182c8298f21246793ca81188ba",
+    "url": "/jarvis/static/media/5.9610ad18.png"
+  },
+  {
+    "revision": "e35884bfa56ada4c6c2255c978dc93a8",
+    "url": "/jarvis/static/media/5.e35884bf.png"
+  },
+  {
+    "revision": "1cbf6c2ab423dc2abb7f71f823eafb1b",
+    "url": "/jarvis/static/media/6.1cbf6c2a.png"
+  },
+  {
+    "revision": "7a5b3fc0c74aee98212d92dadb9af4f3",
+    "url": "/jarvis/static/media/6.7a5b3fc0.png"
+  },
+  {
+    "revision": "28c45a782442b73c8e4596e1bc680768",
+    "url": "/jarvis/static/media/7.28c45a78.png"
+  },
+  {
+    "revision": "cd1d36950b1f91c6d746840aff6fcbd6",
+    "url": "/jarvis/static/media/7.cd1d3695.png"
+  },
+  {
+    "revision": "d99d843f20d0c77ab4f93630b310b290",
+    "url": "/jarvis/static/media/8.d99d843f.png"
+  },
+  {
+    "revision": "e86ae25f2b4cebfc6a3a9d28bc46f78f",
+    "url": "/jarvis/static/media/8.e86ae25f.png"
+  },
+  {
+    "revision": "225065fccba8773f9e2041f0231e3609",
+    "url": "/jarvis/static/media/9.225065fc.png"
+  },
+  {
+    "revision": "5cba2ab36e9aa64350676d65c0515644",
+    "url": "/jarvis/static/media/9.5cba2ab3.png"
+  },
+  {
+    "revision": "385366192a426d4c299b92c45bd9381b",
+    "url": "/jarvis/static/media/AdapterStatus1.38536619.png"
+  },
+  {
+    "revision": "cb44dbbf4f0c025ce205a1f0981c62e9",
+    "url": "/jarvis/static/media/Chart1.cb44dbbf.png"
+  },
+  {
+    "revision": "87f2f0b52b072ebd15e2f9247afc57bd",
+    "url": "/jarvis/static/media/LightColorBody.87f2f0b5.png"
+  },
+  {
+    "revision": "4199166bfb3856456d24991311438f45",
+    "url": "/jarvis/static/media/Map1.4199166b.png"
+  },
+  {
+    "revision": "7819d286244806865fca917c3efef552",
+    "url": "/jarvis/static/media/StateList2.7819d286.png"
+  },
+  {
+    "revision": "5695145e384edeb75d81aabd79983dea",
+    "url": "/jarvis/static/media/StateList4.5695145e.png"
+  },
+  {
+    "revision": "8570c4e0022cdb2725d244810bedfe1f",
+    "url": "/jarvis/static/media/index.js.8570c4e0.flow"
+  },
+  {
+    "revision": "b688a940cfa83093d9bb013919ffc057",
+    "url": "/jarvis/static/media/index.js.b688a940.flow"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/jarvis/static/media/jsoneditor-icons.2b9b4872.svg"
+  },
+  {
+    "revision": "f985e03ce7ef7fe873b027e31263804d",
+    "url": "/jarvis/static/media/paypal.f985e03c.webp"
+  },
+  {
+    "revision": "e3988b38557bf522580562623cecf348",
+    "url": "/jarvis/static/media/zefau.e3988b38.jpg"
+  }
+]);
