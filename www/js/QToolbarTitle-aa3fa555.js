0c3c1380e1216452110dc9faa10028a2671d6d35
Zefau <zefau@mailbox.org>
2022-07-31 00:02:27 +0200

    v3.1.0-alpha.29

---
+++ www/js/QToolbarTitle-aa3fa555.js
@@ -0,0 +1,1 @@
+import{bX as t,c as a,h as l,cg as r}from"./index-6bce915b.js";var n=t({name:"QToolbarTitle",props:{shrink:Boolean},setup(o,{slots:e}){const s=a(()=>"q-toolbar__title ellipsis"+(o.shrink===!0?" col-shrink":""));return()=>l("div",{class:s.value},r(e.default))}});export{n as Q};
--- www/js/index-73ece3e2.js
+++
@@ -1,1 +0,0 @@
-import{dA as a,dB as s}from"./index-2b0597a6.js";function o(t){a(1,arguments);var r=s(t);return r.setHours(0,0,0,0),r}export{o as s};
