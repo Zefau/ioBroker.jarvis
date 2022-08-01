f78ee998682bc8e2a39367ed2e6c542c03938233
Zefau <zefau@mailbox.org>
2022-08-01 23:24:32 +0200

    v3.1.0-alpha.30

--- www/js/___vite-browser-external_commonjs-proxy-0107ae2e.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./_commonjsHelpers-45ea234c.js";var r={},t=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"})),o=e(t);export{o as r};
