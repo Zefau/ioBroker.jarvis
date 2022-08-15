bf0a7d9c7a1345ef4050b31df6b2931c927dccd9
Zefau <zefau@mailbox.org>
2022-08-15 23:08:20 +0200

    v3.1.0-alpha.34

--- www/js/QToolbar-7d45fcfe.js
+++
@@ -1,1 +0,0 @@
-import{bX as t,Q as s,ac as r,cg as n}from"./index-db4a3b0d.js";var c=t({name:"QToolbar",props:{inset:Boolean},setup(o,{slots:a}){const e=s(()=>"q-toolbar row no-wrap items-center"+(o.inset===!0?" q-toolbar--inset":""));return()=>r("div",{class:e.value},n(a.default))}});export{c as Q};
