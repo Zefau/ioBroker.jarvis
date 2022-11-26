081911c7e4399639d79460da3e0ce2b89ecc3fbe
Zefau <zefau@mailbox.org>
2022-11-26 23:25:46 +0100

    v3.1.0

---
+++ www/js/index-b5e8f024.js
@@ -0,0 +1,1 @@
+import{dH as a,dI as s}from"./index-309ef695.js";function o(t){a(1,arguments);var r=s(t);return r.setHours(0,0,0,0),r}export{o as s};
--- www/js/index-d1f44301.js
+++
@@ -1,1 +0,0 @@
-import{dH as a,dI as s}from"./index-3290303e.js";function o(t){a(1,arguments);var r=s(t);return r.setHours(0,0,0,0),r}export{o as s};
